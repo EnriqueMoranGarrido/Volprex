313bf1b90cec9858e331e3e598624671094d4ae9
Enrique Moran <enrique.morango@udlap.mx>
2022-05-03 23:01:39 -0500

    Creation of the Emergency Page, starting the buttons

--- src/screens/EmergencyScreen/EmergencyScreen.js
+++ src/screens/EmergencyScreen/EmergencyScreen.js
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, Pressable } from "react-native";
 import React from "react";
 
 import styles from "./styles";
+import users from "../../../assets/Data/users";
+
+const doctorImage = users.user.doctor.image;
 
 const EmergencyScreen = () => {
   return (
-    <View>
-      <Text>EmergencyScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <Image source={{ uri: doctorImage }} style={styles.image} />
+      </View>
+      <Pressable>
+        <View style={styles.button}>
+          <Text>Holi</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
--- src/screens/EmergencyScreen/styles.js
+++ src/screens/EmergencyScreen/styles.js
@@ -1,5 +1,23 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
-const styles = StyleSheet.create({});
+const Height = Dimensions.get("window").height;
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  imageContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  image: {
+    height: Height / 3,
+    aspectRatio: 3 / 3,
+    borderRadius: 500,
+  },
+  button: {
+    width: "40%",
+    backgroundColor: "red",
+  },
+});
 
 export default styles;
